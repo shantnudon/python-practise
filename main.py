6b2a357298d11e4ef9a84fb400e1126bdcf2c01e
ShantnuDON_laptop <56631328+shantnudon@users.noreply.github.com>
2024-03-06 03:26:20 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# print("yoyo")
+name = "don"
+age = 12
+
+# print(f"my name is {name} and my age is {age}")
+
+
+def factorial(n):
+    if (n==0 or n==1):
+        return 1
+    else:
+        return n * factorial(n-1)    
+
+# print(factorial(5))
+
+dict = {
+    "shantnu" : 24,
+    "don" : 18,
+    "siya" : 19
+}
+
+# print(dict)
+# print(dict.items())
+
+# for value,key in dict.items():
+#     print(f"my name is {key} and my age is {value}")
